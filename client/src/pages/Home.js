6ff9f871fd78e156dafcd18c873f8cd503533c11
Sky K <skyj.kim7@gmail.com>
2023-02-10 13:56:29 -0800

    updated /pages/Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,25 @@
 //importing react and the {useQuery}
-import React from 'react';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import { useQuery } from "@apollo/client";
 
 //importing Qparams from components
-import Qparams from '../components/Qparams';
+import Qparams from "../components/Qparams";
 
-import { queries } from '../utils/queries';
+import { queries } from "../utils/queries";
 
+const Home = () => {
+  const { data } = useQuery(Qparams);
+  const questions = data?.questions || [];
+
+  return (
+    <main>
+      <div className="PH">
+        <div className="PH">
+          <Qparams questions={questions} title="Blast Off!" />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
