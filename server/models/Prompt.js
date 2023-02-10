c5fcc3be92ce392122edcc0831758cf9995e42c7
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2023-02-10 13:38:02 -0800

    create prompt model

---
+++ server/models/Prompt.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+// 
+const promptSchema = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    difficulty: {
+        type: String,
+        required: true
+    },
+    language: {
+        type: String,
+        required: true
+    },
+    question: {
+        type:String,       
+    },
+    response: {
+        type: String,  
+    },
+    feedback: {
+        type: String,
+    },
+});
+
+const Prompt = model('Prompt', promptSchema);
+
+module.exports = Prompt;
