c7b82fbb34905561fa7efe1a0a83cf40d2d46eb8
Sky K <skyj.kim7@gmail.com>
2023-02-10 13:37:41 -0800

    updated Footer

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -10,41 +10,38 @@ const Header = () => {
     Auth.logout();
   };
   return (
-    <header className = "headerPH">
-        <div className = "containerPH">
-            <div>
-                <Link className = "TitleNAVPH">
-                    <h1 className= "titleHeadPh">
-                        GalaQuiz
-                    </h1>
-                </Link>
-                <p className="titleHeadPh">
-                    Take your coding skills to the next LEVEL.
-                </p>
-            </div>
-            <div>
-                {Auth.loggedIn() ? (
-                    <>
-                    <Link className ="PH">
-                        {Auth.getProfile().data.username}'s profile
-                    </Link>
-                    <button className= "PH" onClick ={logout}>
-                        Logout
-                    </button>
-                    </>
-                ) : (
-                    <>
-                    <Link className="PH" to="/login">
-                        Login
-                    </Link>
-                    <Link className="PH" to ="/signup">
-                        Signup
-                    </Link>
-                    </>
-                )}
-            </div>
-
+    <header className="headerPH">
+      <div className="containerPH">
+        <div>
+          <Link className="TitleNAVPH">
+            <h1 className="titleHeadPh">GalaQuiz</h1>
+          </Link>
+          <p className="titleHeadPh">
+            Take your coding skills to the next LEVEL.
+          </p>
         </div>
+        <div>
+          {Auth.loggedIn() ? (
+            <>
+              <Link className="PH">
+                {Auth.getProfile().data.username}'s profile
+              </Link>
+              <button className="PH" onClick={logout}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link className="PH" to="/login">
+                Login
+              </Link>
+              <Link className="PH" to="/signup">
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
     </header>
   );
 };
