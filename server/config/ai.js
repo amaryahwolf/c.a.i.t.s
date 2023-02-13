064b7881cea8693b7f12afb38451f6fa996379b1
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2023-02-13 10:51:35 -0800

    debug AI connection route

--- server/config/ai.js
+++ server/config/ai.js
@@ -2,7 +2,7 @@ const { Configuration, OpenAIApi } = require("openai");
 
 // Import OpenAI node package, creating connection to API, and exporting to the rest of the application.
 const configuration = new Configuration({
-  apiKey: sk - AXsPpJHeexXu9ZQq3otDT3BlbkFJu8c5iMvDi0ClhtExoPiM,
+  apiKey: "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",
 });
 const openAi = new OpenAIApi(configuration);
 
