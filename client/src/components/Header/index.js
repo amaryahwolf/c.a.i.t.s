b93848ece5a17f6f4970e5e0798fa09b34c567bf
Vanessa Chan <vchan@Vanessas-MacBook-Air.local>
2023-02-13 10:11:11 -0800

    spelling  mistake

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -21,7 +21,7 @@ const Header = () => {
               height="80"
               className="d-inline-block align-top"
             />{" "}
-          <Navbar.Brand href="./">BrianBot</Navbar.Brand>
+          <Navbar.Brand href="./">BryanBot</Navbar.Brand>
           {Auth.loggedIn() ? (
             <>
               <Link className="PH">
