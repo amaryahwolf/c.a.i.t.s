6d605a492d54c2ec3fa4dd7ce181f2251a93dcd2
Sky K <skyj.kim7@gmail.com>
2023-02-13 13:59:59 -0800

    updated Home, changed Qparams to Explanations

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,67 @@
 //importing react and the {useQuery}
+import { printIntrospectionSchema } from "graphql";
 import React from "react";
-import { Container, Card } from 'react-bootstrap';
-const Home = () => {
-  
+import { Container } from "react-bootstrap";
+const question = {
+  color: "white",
+  viewHeight: "100%",
+  background: "linear-gradient(#8E2DE2, #4A00E0)",
+  fontFamily: "Arial",
+  textAlign: "center",
+  opacity: "0.4",
+  marginTop: "100px",
+  padding: "50px",
+  opacity: "0.8",
+  borderRadius: "20px",
+  width: "50%",
+  marginLeft: "auto",
+  marginRight: "auto",
+  display: "block",
+  // justifyContent: 'center',
+};
 
+const explanation = {
+  color: "white",
+  viewHeight: "100%",
+  background: "linear-gradient(#b92b27, #1565C0)",
+  fontFamily: "Arial",
+  textAlign: "center",
+  opacity: "0.4",
+  marginTop: "100px",
+  padding: "50px",
+  opacity: "0.8",
+  borderRadius: "20px",
+  width: "50%",
+  marginLeft: "auto",
+  marginRight: "auto",
+  display: "block",
+  //justifyContent: 'center',
+};
+
+const containerStyle = {
+  opacity: "0.8",
+  background: "transparent",
+  display: "flex",
+  marginLeft: "auto",
+  marginRight: "auto",
+};
+
+const Home = () => {
   return (
     <>
-      <Container fluid>
-        <p>Welcome baack~</p>
-      </Container>
-      <Container fluid>
-      <Card fluid>
-      <Card.Body>This is a placeholder for the main app</Card.Body>
-    </Card>
+      <Container style={containerStyle}>
+        <Container fluid>
+          <input
+            type="text"
+            placeholder="question box"
+            style={question}></input>
+        </Container>
+        <Container fluid>
+          <input
+            type="text"
+            placeholder="explanation box"
+            style={explanation}></input>
+        </Container>
       </Container>
     </>
   );
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,4 +1,6 @@
+
 import React from "react";
+
 
 const Profile = () => {
   return (
